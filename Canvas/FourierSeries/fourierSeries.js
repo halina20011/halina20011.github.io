121334e06a7085912863ac8ebede672f3c558428
Mario Durakovic <mariodurakovic99@gmail.com>
2022-11-07 18:19:33 +0100

    Improving code quality

--- Canvas/FourierSeries/fourierSeries.js
+++ Canvas/FourierSeries/fourierSeries.js
@@ -1,23 +1,136 @@
+import {DOWNLOADER} from "/Canvas/download.js";
+
 let canvas = document.getElementById("my-canvas");
 let context = canvas.getContext("2d");
 
 let image = context.createImageData(canvas.width, canvas.height);
 let data = image.data;
 
+let downloader = new DOWNLOADER(canvas);
+
 let addButton = document.getElementById("addButton");
-addButton.addEventListener("click", function() {add(); update();}, false);
+addButton.addEventListener("click", function() {add();}, false);
 
 let removeButton = document.getElementById("removeButton");
-removeButton.addEventListener("click", function() {remove(waveInfo.length - 1); update();}, false);
+removeButton.addEventListener("click", function() {remove();}, false);
 
-let downloadImageButton = document.getElementById("downloadImage")
-downloadImageButton.addEventListener("click", () => {downloadImage();}, false);
+let downloadImageButton = document.getElementById("downloadImage");
+downloadImageButton.addEventListener("click", function() { downloader.downloadImage(); }, false);
 
-function update(){
+function timelapsee(fArguments){
+    let from = parseInt(fArguments[0].value);
+    let to = parseInt(fArguments[1].value);
+    let clear = fArguments[2].checked;
+
+    if(clear * 0 == 0){
+        waveBuffer = [];
+    }
+
+    if(from * 0 == 0 && to * 0 == 0){
+        angle = from;
+        console.log(`From: ${from}, to: ${to}.`);
+        for(let i = from; i < to - from; i++){
+            angle += angleAdd;
+            main();
+            downloader.addImage();
+            console.log(`Number of levels currently running: ${angle}.`);
+        }
+        downloader.downloadAll();
+    }
     waveBuffer = [];
+}
 
+let timelapseInputData = {
+    "function": timelapsee,
+    "arguments": [{
+            "text": {"element": "p", "innerHTML": "Steps: "},
+            "inputFrom": {"element": "input", "type": "number", "from": 0, "value": 0},
+            "inputTo": {"element": "input", "type": "number", "from": 1, "value": 1}
+        },
+        {
+            "text": {"element": "p", "innerHTML": "Clear window: "},
+            "clearWindow": {"element": "input", "type": "checkbox"}
+        }
+    ]
+}
+
+let timelapse = downloader.timelapse(timelapseInputData);
+
+let waveHolder = document.getElementById("waveHolder");
+
+let level = 1;
+let waveBuffer = [];
+
+function updateInfo(){
+    clear();
+    waveBuffer = [];
+    
+    let count = waveHolder.childElementCount;
+    for(let i = 0; i < count; i++){
+        // console.log(`Removing: ${i} ${count}.`);
+        waveHolder.children[0].remove();
+    }
+
+    for(let i = 0; i < level; i++){
+        // console.log(`Adding: ${i}.`);
+    
+        let waveSettingsElement = document.createElement("div");
+        waveSettingsElement.className = "waveInfo";
+        waveSettingsElement.id = "waveSettings1"
+    
+        let row1 = document.createElement("div");
+        row1.className = "row1"
+        let row2 = document.createElement("div");
+        row2.className = "row2"
+        let row3 = document.createElement("div");
+        row3.className = "row3"
+    
+        waveSettingsElement.appendChild(row1);
+        waveSettingsElement.appendChild(row2);
+        waveSettingsElement.appendChild(row3);
+    
+        let indexName = document.createElement("p");
+        indexName.innerHTML = `Circle: ${i}`;
+        row1.appendChild(indexName);
+    
+        // Radius
+        let radiusHolder = document.createElement("div");
+        radiusHolder.className = "radiusHolder";
+        row2.appendChild(radiusHolder);
+        
+        // Radius Text
+        let radiusText = document.createElement("div");
+        radiusText.innerHTML = "Radius: "
+        
+        // Radius value
+        let radius = document.createElement("div");
+        radius.innerHTML = 4 / (Math.PI * (2 * i + 1));
+    
+        // Append 
+        radiusHolder.appendChild(radiusText);
+        radiusHolder.appendChild(radius);
+    
+        waveHolder.appendChild(waveSettingsElement);    
+    }
+
+    updatePosition();
+}
+
+function add(){
+    level++;
+    updateInfo();
+}
+
+function remove(){
+    if(1 < level){
+        level--;
+        updateInfo();
+    }
+}
+
+function updatePosition(){
     drawPoint = 0;
-    for(let i = 0; i < waveInfo.length; i++){
+    for(let i = 0; i < level; i++){
         let _i = i * 2 + 1;
         drawPoint += multiply * (4 / (Math.PI * _i));
     }
@@ -25,12 +138,7 @@ function update(){
     drawPoint += 5;
 }
 
-let waveHolder = document.getElementById("waveHolder");
-
-let waveInfo = [];
-let waveBuffer = [];
-
-let multiply = canvas.width / 10; 
+let multiply = canvas.width / 10;
 let drawPoint = 0;
 
 let angle = Math.PI;
@@ -42,17 +150,18 @@ function swapBuffer(){
 
 let xOffset = 0; let yOffset = 0;
 
-function drawPixel(x, y, r, g, b, a){
+function drawPixel(x, y, r = 255, g = 255, b = 255, a = 255){
     let xRounded = Math.round(x + xOffset);
     let yRounded = Math.round(y + yOffset);
     
-    if(xRounded < canvas.width && yRounded < canvas.height){
+    if(0 < xRounded && xRounded < canvas.width && 0 < yRounded && yRounded < canvas.height){
         let index = 4 * (canvas.width * yRounded + xRounded);
         data[index + 0] = r;
         data[index + 1] = g;
         data[index + 2] = b;
         data[index + 3] = a;
     }
+
 }
 
 function swap(i1, i2){
@@ -122,7 +231,7 @@ function clear(){
     }
 }
 
-function loop(){
+function main(){
     clear();
     let prevX = 0;
     let prevY = 0;
@@ -132,14 +241,9 @@ function loop(){
 
     translate(canvas.width / 4, canvas.height / 2);
 
-    for (let i = 0; i < waveInfo.length; i++) {
+    for(let i = 0; i < level; i++){
         let _i = i * 2 + 1;
         let r = multiply * (4 / (Math.PI * _i));
-
-        if(waveInfo[i].wasUpdated == false){
-            waveInfo[i].radius = r;
-            waveInfo[i].update();
-        }
 
         drawCircle(prevX, prevY, r);
         
@@ -150,7 +254,7 @@ function loop(){
         // Draw Point that is on the circle
         let pointR = 4;
         // if it is last circle then drow a point
-        if(i == waveInfo.length - 1){
+        if(i == level - 1){
             for(let i = 1; i < pointR; i++){
                 drawCircle(x, y, i);
             }
@@ -167,145 +271,12 @@ function loop(){
     for(let i = 0; i < waveBuffer.length - 1; i++){
         drawLine(i + drawPoint, waveBuffer[i], i + drawPoint + 1, waveBuffer[i + 1]);
     }
-
-    angle += angleAdd;
+    swapBuffer();
 }
 
-let donwloaded = false;
+updateInfo();
 
 setInterval(() => {
-    loop(); 
-    swapBuffer();
-    downloadImages();
-}, 10)
-    
-/* ############################################################################################### */
-/* ####################################        Settings       #################################### */
-/* ############################################################################################### */
-    
-class WAVESETTING {
-    constructor(index, radiusElement){
-        this.index = index;
-        this.radius = null;
-
-        this.radiusElement = radiusElement
-        this.wasUpdated = false;
-    }
-    
-    update(){
-        this.radiusElement.innerHTML = this.radius;
-        this.wasUpdated == true;
-    }
-}
-
-add();
-update();
-
-function add(){
-    let newIndex = waveInfo.length;
-
-    let waveSettingsElement = document.createElement("div");
-    waveSettingsElement.className = "waveInfo";
-    waveSettingsElement.id = "waveSettings1"
-
-    let row1 = document.createElement("div");
-    row1.className = "row1"
-    let row2 = document.createElement("div");
-    row2.className = "row2"
-    let row3 = document.createElement("div");
-    row3.className = "row3"
-
-    waveSettingsElement.appendChild(row1);
-    waveSettingsElement.appendChild(row2);
-    waveSettingsElement.appendChild(row3);
-
-    let indexName = document.createElement("p");
-    indexName.innerHTML = `Circle: ${newIndex}`;
-    row1.appendChild(indexName);
-
-    // Radius
-    let radiusHolder = document.createElement("div");
-    radiusHolder.className = "radiusHolder";
-    row2.appendChild(radiusHolder);
-    
-    // Radius Text
-    let radiusText = document.createElement("div");
-    radiusText.innerHTML = "Radius: "
-    
-    // Radius value
-    let radius = document.createElement("div");
-    radius.innerHTML = 10;
-
-    // Append 
-    radiusHolder.appendChild(radiusText);
-    radiusHolder.appendChild(radius);
-
-    waveHolder.appendChild(waveSettingsElement);
-
-    waveInfo.push(new WAVESETTING(newIndex, radius));
-}
-
-function remove(index){
-    waveHolder.children[index].remove()
-    waveInfo.splice(index, 1);
-}
-
-/* ############################################################################################### */
-/* ##################################    Downloading    ########################################## */
-/* ############################################################################################### */
-
-let toggleDownload = document.getElementById("toggleDownload");
-
-toggleDownload.addEventListener("click", () => {
-    downloading = !downloading;
-    if(downloading){
-        toggleDownload.className = "pauseButton";
-    }
-    else{
-        toggleDownload.className = "playButton";
-    }
-}, false);
-
-var imagesFile = {imagesBase64: []}
-
-var downloading = false;
-
-var filename = "imagesBase64.json"
-
-function downloadAll(){
-    stringJsonImagesFile =  JSON.stringify(imagesFile);
-    download(generateTextFileUrl(stringJsonImagesFile), filename);
-    imagesFile.imagesBase64 = [];
-}
-
-function downloadImages(){
-    if(downloading == true){
-        console.log("");
-        var dataURL = canvas.toDataURL("image/jpeg", 1.0);
-        imagesFile.imagesBase64.push(dataURL);
-    }
-}
-
-function downloadImage(){
-    var dataURL = canvas.toDataURL("image/jpeg", 1.0);
-    downloadFile(dataURL, `fourierSeries_${Date.now()}.jpeg`);
-}
-
-function generateTextFileUrl(txt) {
-    var textFileUrl = null;
-    let fileData = new Blob([txt], {type: 'text/plain'});
-    if (textFileUrl !== null) {
-        window.URL.revokeObjectURL(textFile);
-    }
-    textFileUrl = window.URL.createObjectURL(fileData);
-    return textFileUrl;
-};
-
-
-function downloadFile(data, filename = 'untitled.jpeg') {
-    var a = document.createElement('a');
-    a.href = data;
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-}
+    main(); 
+    angle += angleAdd;
+}, 10);
