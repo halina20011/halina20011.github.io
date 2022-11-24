68276b7317136d906e090c391808b4f539fc680c
Mario Durakovic <mariodurakovic99@gmail.com>
2022-11-24 06:17:03 +0100

    head.js code cleaning and bug fix

--- head.js
+++ head.js
@@ -1,11 +1,10 @@
-var column_up = document.getElementById("columnUp");
-var column_display = document.getElementById("columnDisplay");
-var contentTable = document.getElementById("table");
+let columnUp = document.getElementById("columnUp");
+let contentTable = document.getElementById("pageContentContainer");
 
-window.addEventListener('scroll', function() {scroll();}, false );
+window.addEventListener('scroll', () => { scroll(); }, false);
 
-var newValue = 0;
-var oldValue = 0;
+let newValue = 0;
+let oldValue = 0;
 
 let height = 36;
 
@@ -22,36 +21,16 @@ function scroll(){
 
 function moveHead(direction){
     if(direction == "Down"){ 
-        column_up.style.top = `-${height}px`;
-        if(contentTable) { // If contentTable is in document then move it too
+        columnUp.style.top = `-${height}px`;
+        if(contentTable){ // If contentTable is in document then move it too
             contentTable.style.top = `${height}px`;
         }
     } 
     else if(direction == "Up"){
-        column_up.style.top = "0px"; 
+        columnUp.style.top = "0px"; 
         if(contentTable){
             contentTable.style.top = `${height * 2}px`;
             
         } 
     }
-}
-
-function moveTopObject(_el, _start, _stop, _time, _units){
-    _el.animate([{ top: _start+_units}, { top: _stop + _units}], _time).onfinish = function(){
-        _el.style.top = _stop + _units; // block the element transition to the '_stop' position
-    };
-}
-
-function toPercentage(element, type = "width"){
-    var width = 0;
-    if(type == "width"){
-        width = window.document.documentElement.clientWidth;
-    }
-    else{
-        width = window.document.documentElement.clientHeight;
-    }
-    var elementPosition = element;
-    // console.log(element)
-    var valueInPercentege = Math.round(elementPosition / width * 100);
-    return valueInPercentege;
 }
