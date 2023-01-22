17ada0cfb71b1cd2abca51ee362e7f5c280ef520
Mario Durakovic <mariodurakovic99@gmail.com>
2023-01-22 17:32:14 +0100

    Code link

--- Tools/codeLink.js
+++ Tools/codeLink.js
@@ -1,10 +1,45 @@
 let codeLink = document.getElementById("codeLink");
 
+let jsFileVersion = ["/Canvas/"];
+
 function getCodeLink(){
     let filePath = document.location.pathname;
-    console.log(filePath);
+    // console.log(document.location);
+    
     filePath = (filePath != "/") ? filePath : "/index.html";
-    codeLink.href = `https://github.com/halina20011/halina20011.github.io/blob/main${filePath}`;
+
+    if(-1 < filePath.search("/Canvas/")){
+        let urls = filePath.split("/");
+        // console.log(urls);
+
+        for(let i = 0; i < urls.length; i++){
+            if(urls[i] == ''){
+                urls.splice(i, 1);
+            }
+        }
+        
+        let absPath = "";
+        let htmlFileName = "";
+
+        for(let i = 0; i < urls.length; i++){
+            if(i != (urls.length - 1)){
+                absPath += `/${urls[i]}`;
+            }
+            else{
+                htmlFileName = urls[i];
+            }
+        }
+
+        console.log(`Path ${absPath} ${htmlFileName}`);
+        // console.log(htmlFileName);
+
+        fileName = htmlFileName.split(".")[0];
+
+        codeLink.href = `https://github.com/halina20011/halina20011.github.io/blob/main${absPath}/${fileName}.js`;
+    }
+    else{
+        codeLink.href = `https://github.com/halina20011/halina20011.github.io/blob/main${filePath}`;
+    }
 }
 
 getCodeLink();
