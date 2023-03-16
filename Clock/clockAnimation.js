196810611501c8befad4b2f43365006426e6ed40
Mario Durakovic <mariodurakovic99@gmail.com>
2023-03-16 22:24:30 +0100

    Clock fix

--- Clock/clockAnimation.js
+++ Clock/clockAnimation.js
@@ -3,17 +3,23 @@ function analogClock(){
     let arrowMinutes = document.getElementsByClassName("arrowMinutes")[0];
     let arrowSeconds = document.getElementsByClassName("arrowSeconds")[0];
 
-    let rtClock = new Date();
+    let data = new Date();
     
-    let hours = rtClock.getHours();
-    let minutes = rtClock.getMinutes();
-    let seconds = rtClock.getSeconds();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     
-    hours = (hours > 12) ? hours - 12 : hours;
-    hoursArrowRotation = minutes * 6;
-    minutesArrowRotation = hours * 30 + hoursArrowRotation * 0.06;
-    secondsArrowRotation = seconds * 6;
+    hours = (12 < hours) ? hours - 12 : hours;
     
+    // degree/minutes => 360 / 60 => 6
+    // degree/hours   => 360 / 12 = 30
+    let minutesArrowRotation = minutes * 6;
+    let hoursArrowRotation = hours * 30 + minutes * 0.5;
+    let secondsArrowRotation = seconds * 6;
+
+    // console.log(hours, minutes);
+    // console.log(hoursArrowRotation, minutesArrowRotation);
+
     arrowHours.style.transform = "rotate(" + hoursArrowRotation +"deg)";
     arrowMinutes.style.transform = "rotate(" + minutesArrowRotation +"deg)";
     arrowSeconds.style.transform = "rotate(" + secondsArrowRotation +"deg)";
